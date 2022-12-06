bb93ca1b6ae11e59c6bab8347f19a037d02b6c55
hidekiabe <hideki-abe@hotmail.com>
2022-12-06 08:59:09 -0300

    correção

--- evolution/src/test/java/com/api/evolution/EvolutionApplicationTests.java
+++ evolution/src/test/java/com/api/evolution/EvolutionApplicationTests.java
@@ -1,13 +1,13 @@
-package br.com.evolution.api;
+package com.api.evolution;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
-class ApiApplicationTests {
+class EvolutionApplicationTests {
 
-	@Test
-	void contextLoads() {
-	}
+    @Test
+    void contextLoads() {
+    }
 
 }
